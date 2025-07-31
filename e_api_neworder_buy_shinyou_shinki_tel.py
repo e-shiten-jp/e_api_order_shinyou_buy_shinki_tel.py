d75e2a8fb52d7d0571f0c501f2229da74d3e904f
e-shiten-jp <86388761+e-shiten-jp@users.noreply.github.com>
2025-07-31 18:15:53 +0900

    Update e_api_neworder_buy_shinyou_shinki_tel.py

--- e_api_neworder_buy_shinyou_shinki_tel.py
+++ e_api_neworder_buy_shinyou_shinki_tel.py
@@ -22,12 +22,9 @@
 #
 #
 # 利用方法: 
-# 事前に「e_api_login_tel.py」を実行して、仮想URL等を取得しておいてください。
-#
-#
-# 利用方法: 
 # 事前に「e_api_login_tel.py」を実行して、
 # 仮想URL（1日券）等を取得しておいてください。
+# 「e_api_login_tel.py」と同じディレクトリで実行してください。
 #
 #
 # == ご注意: ========================================
@@ -248,7 +245,20 @@ def func_read_from_file(str_fname):
                 str_read = str_read + line
         return str_read
     except IOError as e:
-        print('Can not Write!!!')
+        print('ファイルを読み込めません!!! ファイル名：',str_fname)
+        print(type(e))
+
+
+# 機能: ファイルに書き込む
+# 引数1: 出力ファイル名
+# 引数2: 出力するデータ
+# 備考:
+def func_write_to_file(str_fname_output, str_data):
+    try:
+        with open(str_fname_output, 'w', encoding = 'utf-8') as fout:
+            fout.write(str_data)
+    except IOError as e:
+        print('ファイルに書き込めません!!!  ファイル名：',str_fname_output)
         print(type(e))
 
 
@@ -387,19 +397,6 @@ def func_get_p_no(fname, class_login_property):
     class_login_property.p_no = int(json_p_no_info.get('p_no'))
         
     
-# 機能: ファイルに書き込む
-# 引数1: 出力ファイル名
-# 引数2: 出力するデータ
-# 備考:
-def func_write_to_file(str_fname_output, str_data):
-    try:
-        with open(str_fname_output, 'w', encoding = 'utf-8') as fout:
-            fout.write(str_data)
-    except IOError as e:
-        print('Can not Write!!!')
-        print(type(e))
-
-
 # 機能: p_noを保存するためのjson形式のテキストデータを作成します。
 # 引数1: p_noを保存するファイル名（fname_info_p_no = "e_api_info_p_no.txt"）
 # 引数2: 保存するp_no
@@ -724,11 +721,10 @@ if __name__ == "__main__":
     my_sCondition = '0'    # 13.執行条件。  0:指定なし、2:寄付、4:引け、6:不成。指し値は、0:指定なし。
     my_sOrderPrice = '000'   # 14.注文値段。  *:指定なし、0:成行、上記以外は、注文値段。小数点については、関連資料:「立花証券・e支店・API、REQUEST I/F、マスタデータ利用方法」の「2-12. 呼値」参照。
     my_sOrderSuryou = '100'  # 15.注文数量。
-    
     # --- 以上設定項目 -------------------------------------------------------------------------
 
 
-    # --- ファイル名等を設定 ------------------------------------------------------------------
+    # --- ファイル名等を設定（実行ファイルと同じディレクトリ） ---------------------------------------
     fname_account_info = "e_api_account_info.txt"
     fname_login_response = "e_api_login_response.txt"
     fname_info_p_no = "e_api_info_p_no.txt"
